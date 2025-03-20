5472bbfe5043472c3c5acf44e28f7de9b2f04a2b
seoulcircle <dlghd1997@naver.com>
2025-03-20 21:53:34 +0900

    배포

--- src/Pages/DayPalette/DayUtils.ts
+++ src/Pages/DayPalette/DayUtils.ts
@@ -12,7 +12,7 @@ export const getRGBA = (
   );
   const G = Math.min(255, Math.max(0, (humidity / 100) * 255));
   const B = Math.abs(255 - (hour / 24) * 255);
-  const A = Math.min(1, 1 - pm / 100).toFixed(2);
+  const A = Math.min(1, 0.8 - pm / 100).toFixed(2);
 
   return `rgba(${Math.round(R)}, ${Math.round(G)}, ${Math.round(B)}, ${A})`;
 };
